4296405a0a9a5ad4641d7fce79f9ae0ff9f5d1d9
TehBrian <tehbrian@outlook.com>
2020-11-25 10:30:34 -0500

    Use hex color codes

--- src/main/java/xyz/tehbrian/buildersutilities/util/MessageUtils.java
+++ src/main/java/xyz/tehbrian/buildersutilities/util/MessageUtils.java
@@ -1,17 +1,30 @@
 package xyz.tehbrian.buildersutilities.util;
 
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
 import xyz.tehbrian.buildersutilities.BuildersUtilities;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/* TODO Use adventure. */
 public class MessageUtils {
+
+    public static final Pattern HEX_PATTERN = Pattern.compile("&(#[A-Fa-f0-9]{6})");
 
     private MessageUtils() {
     }
 
     public static String color(String string) {
-        return string == null ? null : ChatColor.translateAlternateColorCodes('&', string);
+        return string == null ? null : replaceHex(ChatColor.translateAlternateColorCodes('&', string));
+    }
+
+    public static String replaceHex(String str) {
+        Matcher matcher = HEX_PATTERN.matcher(str);
+        while (matcher.find()) {
+            str = str.replace(matcher.group(0), ChatColor.of(matcher.group(1)).toString());
+        }
+        return str;
     }
 
     public static String getMessage(String configKey) {
