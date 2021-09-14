8b5d5e476fa7d507e920faefc17671040852e22f
TehBrian <tehbrian@protonmail.com>
2021-09-13 20:03:12 -0400

    make Permissions less redundant

--- src/main/java/xyz/tehbrian/buildersutilities/Constants.java
+++ src/main/java/xyz/tehbrian/buildersutilities/Constants.java
@@ -33,19 +33,21 @@ public final class Constants {
      */
     public static final class Permissions {
 
-        public static final String ADVANCED_FLY = "buildersutilities.advancedfly";
-        public static final String NIGHT_VISION = "buildersutilities.nightvision";
-        public static final String NO_CLIP = "buildersutilities.noclip";
+        public static final String ROOT = "buildersutilities";
 
-        public static final String IRON_DOOR_TOGGLE = "buildersutilities.irondoortoggle";
-        public static final String DOUBLE_SLAB_BREAK = "buildersutilities.doubleslabbreak";
-        public static final String GLAZED_TERRACOTTA_ROTATE = "buildersutilities.glazedterracottarotate";
+        public static final String ADVANCED_FLY = ROOT + ".advancedfly";
+        public static final String NIGHT_VISION = ROOT + ".nightvision";
+        public static final String NO_CLIP = ROOT + ".noclip";
 
-        public static final String RC = "buildersutilities.rc";
-        public static final String RELOAD = "buildersutilities.reload";
-        public static final String SPECTATE_TELEPORT = "buildersutilities.spectate.teleport";
-        public static final String SPECTATE_ENTITY = "buildersutilities.spectate.entity";
-        public static final String SPECTATE_PLAYER = "buildersutilities.spectate.player";
+        public static final String IRON_DOOR_TOGGLE = ROOT + ".irondoortoggle";
+        public static final String DOUBLE_SLAB_BREAK = ROOT + ".doubleslabbreak";
+        public static final String GLAZED_TERRACOTTA_ROTATE = ROOT + ".glazedterracottarotate";
+
+        public static final String RC = ROOT + ".rc";
+        public static final String RELOAD = ROOT + ".reload";
+        public static final String SPECTATE_TELEPORT = ROOT + ".spectate.teleport";
+        public static final String SPECTATE_ENTITY = ROOT + ".spectate.entity";
+        public static final String SPECTATE_PLAYER = ROOT + ".spectate.player";
 
         private Permissions() {
         }
