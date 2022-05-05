cfd84ec4025ccdf90613d3903d73a6e08a554b95
TehBrian <tehbrian@protonmail.com>
2022-05-04 23:16:13 -0400

    perf: use distance squared in #isTop

--- src/main/java/xyz/tehbrian/buildersutilities/util/BlockUtil.java
+++ src/main/java/xyz/tehbrian/buildersutilities/util/BlockUtil.java
@@ -7,13 +7,27 @@ import org.bukkit.util.Vector;
 
 public class BlockUtil {
 
+    private static final double MARCH_AMOUNT = 0.05D;
+    private static final double MAX_REACH = 6.0D;
+    private static final double MAX_REACH_SQUARED = Math.pow(MAX_REACH, 2);
+
     public static boolean isTop(final Player player, final Block block) {
         final Location ray = player.getEyeLocation().clone();
-        final Vector marchAmount = player.getEyeLocation().getDirection().multiply(0.05D);
-        while (!ray.getBlock().equals(block) && ray.distance(player.getEyeLocation()) < 6.0D) {
-            ray.add(marchAmount);
+        final Vector march = player.getEyeLocation().getDirection().multiply(MARCH_AMOUNT);
+        while (!ray.getBlock().equals(block) && ray.distanceSquared(player.getEyeLocation()) < MAX_REACH_SQUARED) {
+            ray.add(march);
         }
         return ray.getY() % 1.0D > 0.5D;
     }
 
+    /*
+     final double y = ray.getY();
+     final double internalY = Math.abs(y % 1.0D);
+
+     if (y > 0) {
+     return internalY > 0.5D;
+     } else {
+     return internalY < 0.5D;
+     }
+     */
 }
