7ed219a55c2d44ec26b8c6fb120aa146c2b20e8f
TehBrian <tehbrian@proton.me>
2023-06-11 10:34:06 -0400

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
   id("java")
   id("com.github.johnrengelman.shadow") version "8.1.1"
   id("io.papermc.paperweight.userdev") version "1.5.5"
-  id("net.kyori.indra.checkstyle") version "3.0.1"
+  id("net.kyori.indra.checkstyle") version "3.1.1"
   id("xyz.jpenilla.run-paper") version "2.1.0"
-  id("com.github.ben-manes.versions") version "0.46.0"
+  id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 group = "xyz.tehbrian"
@@ -22,11 +22,11 @@ repositories {
 }
 
 dependencies {
-  paperDevBundle("1.19.4-R0.1-SNAPSHOT")
+  paperweight.paperDevBundle("1.19.4-R0.1-SNAPSHOT")
 
   implementation("broccolai.corn:corn-minecraft-paper:3.2.0")
   implementation("cloud.commandframework:cloud-minecraft-extras:1.8.3")
-  implementation("com.google.inject:guice:5.1.0")
+  implementation("com.google.inject:guice:7.0.0")
   implementation("dev.tehbrian:tehlib-paper:0.4.2")
   implementation("org.spongepowered:configurate-yaml:4.1.2")
   implementation("dev.tehbrian.restrictionhelper:restrictionhelper-spigot:0.3.4")
