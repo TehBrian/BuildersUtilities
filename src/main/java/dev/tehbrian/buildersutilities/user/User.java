9044622edf961ef72d1e01e8ab9b8ad49c7fdcdd
TehBrian <tehbrian@proton.me>
2023-10-28 19:52:55 -0400

    use infinite duration for night vision effect

--- src/main/java/dev/tehbrian/buildersutilities/user/User.java
+++ src/main/java/dev/tehbrian/buildersutilities/user/User.java
@@ -83,7 +83,7 @@ public final class User {
     }
 
     if (nightVisionEnabled) {
-      player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0, true, false));
+      player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, PotionEffect.INFINITE_DURATION, 0, true, false, false));
     } else {
       player.removePotionEffect(PotionEffectType.NIGHT_VISION);
     }
