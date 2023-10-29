66bb437bd3f2d967a82ea8297fe91d8f8a610fe5
TehBrian <tehbrian@proton.me>
2023-10-29 15:00:10 -0400

    add all light levels to special items menu
    non-op players can't change light blocks

--- src/main/java/dev/tehbrian/buildersutilities/config/SpecialConfig.java
+++ src/main/java/dev/tehbrian/buildersutilities/config/SpecialConfig.java
@@ -4,7 +4,9 @@ import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import dev.tehbrian.tehlib.configurate.AbstractConfig;
 import org.bukkit.Material;
+import org.bukkit.block.data.type.Light;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockDataMeta;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.spongepowered.configurate.CommentedConfigurationNode;
@@ -13,6 +15,7 @@ import org.spongepowered.configurate.ConfigurateException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 public final class SpecialConfig extends AbstractConfig<YamlConfigurateWrapper> {
@@ -41,7 +44,40 @@ public final class SpecialConfig extends AbstractConfig<YamlConfigurateWrapper> 
       return;
     }
 
-    for (final String itemName : itemNames) {
+    for (final String dirtyItemName : itemNames) {
+      final var itemName = dirtyItemName.strip().toUpperCase(Locale.ROOT);
+
+      // special case for light levels.
+      if (itemName.startsWith("LIGHT") && !itemName.equals("LIGHT")) {
+        int level;
+        try {
+          level = Integer.parseInt(itemName.split("-")[1]);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+          this.logger.warn("Invalid light data {}.", itemName);
+          this.logger.warn("Skipping this item. Please check your {}", fileName);
+          this.logger.warn("Printing stack trace:", e);
+          continue;
+        }
+
+        if (level > 15) {
+          level = 15;
+        }
+        if (level < 0) {
+          level = 0;
+        }
+
+        // create a light item with that light level.
+        final ItemStack item = new ItemStack(Material.LIGHT);
+        final BlockDataMeta meta = (BlockDataMeta) item.getItemMeta();
+        final Light data = (Light) Material.LIGHT.createBlockData();
+        data.setLevel(level);
+        meta.setBlockData(data);
+        item.setItemMeta(meta);
+
+        this.items.add(item);
+        continue;
+      }
+
       final Material itemMaterial;
       try {
         itemMaterial = Material.valueOf(itemName);
